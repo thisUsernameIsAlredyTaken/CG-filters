010523c7cb10168181850eaec80bf772d923b3c6
Живаев Артем Евгеньевич <zhivaev.a@ccam.cmc.unn.net>
2019-02-18 10:30:21 +0300

    process Image method

--- CG-filters/Class1.cs
+++ CG-filters/Class1.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace CG_filters
 {
   abstract class Filter
   {
+    public Bitmap processImage(Bitmap sourceImage)
+    {
+      Bitmap resultImage = new Bitmap(sourceImage.Width, sourceImage.Height);
+
+      return resultImage;
+    }
   }
 }
